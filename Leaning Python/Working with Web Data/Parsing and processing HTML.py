aba5846ad46215b342387366186babbbfe996b11
Aantu Centos <aantu014@fiu.edu>
2020-09-24 02:13:05 -0400

    Testing push

---
+++ Leaning Python/Working with Web Data/Parsing and processing HTML.py
@@ -0,0 +1,12 @@
+# import the HTMLParser module
+# in Python 3 you need to import from html.parser
+from html.parser import HTMLParser
+
+def main():
+  # instantiate the parser and feed it some HTML
+  parser = MyHTMLParser()
+    
+
+if __name__ == "__main__":
+  main();
+  
